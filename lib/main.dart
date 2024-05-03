f3025c2e070b723a704dea57f5a30c2775ecf97c
Felipe Franco <francozuluaga97@gmail.com>
2024-05-03 11:01:23 -0500

    BD, Consulta y Mejoras

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatefulWidget {
@@ -17,11 +22,45 @@ class _MyAppState extends State<MyApp> {
   List<dynamic> drivers = [];
   bool showButton = true;
 
-  String year = "";
+  String selectedYear = '1950';
   int round = 0;
 
-  final TextEditingController _yearController = TextEditingController();
-  //final TextEditingController _roundController = TextEditingController();
+  List<String> years = [for (var i = 1950; i <= 2023; i += 1) '$i'];
+
+  late Future<Database> database;
+
+  @override
+  void initState() {
+    super.initState();
+    initDatabase();
+  }
+
+  Future<void> initDatabase() async {
+    database = openDatabase(
+      join(await getDatabasesPath(), 'drivers_database.db'),
+      onCreate: (db, version) {
+        return db.execute(
+          "CREATE TABLE drivers(year TEXT, name TEXT, nationality TEXT)",
+        );
+      },
+      version: 1,
+    );
+  }
+
+  Future<void> insertDriver(Map<String, dynamic> driver) async {
+    final Database db = await database;
+
+    await db.insert(
+      'drivers',
+      driver,
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Map<String, dynamic>>> getDrivers() async {
+    final Database db = await database;
+    return await db.query('drivers');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,52 +80,122 @@ class _MyAppState extends State<MyApp> {
                 child: Column(
                   children: [
                     const SizedBox(height: 16.0),
-                    TextField(
-                      controller: _yearController,
-                      decoration: const InputDecoration(
-                        labelText: 'Año',
-                      ),
+                    DropdownButton<String>(
+                      value: selectedYear,
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedYear = newValue!;
+                        });
+                      },
+                      items: years.map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
                     ),
                     const SizedBox(height: 16.0),
-                    /* TextField(
-                      controller: _roundController,
-                      decoration: const InputDecoration(
-                        labelText: 'Ronda',
-                      ),
-                    ), */
                     ElevatedButton(
                       onPressed: () async {
                         setState(() {
                           isLoading = true;
                         });
-                
-                        year = _yearController.text;
-                        // round = int.parse(_roundController.text);
-                
-                        final HttpLink httpLink = HttpLink('https://rest-to-graphql-api-f1.vercel.app/graphql');
-                
+
+                        final HttpLink httpLink = HttpLink(
+                            'https://rest-to-graphql-api-f1.vercel.app/graphql');
+
                         final GraphQLClient client = GraphQLClient(
                           link: httpLink,
                           cache: GraphQLCache(),
                         );
-                
+
                         final QueryResult result = await client.query(
                           QueryOptions(
                             document: gql(getF1Query),
                             variables: {
-                              'year': year,
+                              'year': selectedYear,
                               'round': 1,
                             },
                           ),
                         );
-                
+
                         setState(() {
                           isLoading = false;
                           drivers = result.data!['driversYearAndRound'];
                           showButton = false;
+
+                          for (var driver in drivers) {
+                            insertDriver({
+                              'year': selectedYear,
+                              'name': driver['name'],
+                              'nationality': driver['nationality'],
+                            });
+                          }
                         });
                       },
                       child: const Text('Continuar'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        final driversFromDb = await getDrivers();
+
+                        if (mounted) {
+                          showDialog(
+                            // ignore: use_build_context_synchronously
+                            context: context,
+                            builder: (BuildContext context) {
+                              return AlertDialog(
+                                title: const Text('Información DB'),
+                                content: SizedBox(
+                                  width: double.maxFinite,
+                                  height:
+                                      MediaQuery.of(context).size.height * 0.7,
+                                  child: GridView.builder(
+                                    shrinkWrap: true,
+                                    gridDelegate:
+                                        const SliverGridDelegateWithFixedCrossAxisCount(
+                                      crossAxisCount: 1,
+                                      mainAxisSpacing: 3,
+                                      crossAxisSpacing: 3,
+                                      childAspectRatio:
+                                          5,
+                                    ),
+                                    itemCount: driversFromDb.length,
+                                    itemBuilder: (context, index) {
+                                      final driver = driversFromDb[index];
+                                      return Row(
+                                        children: [
+                                          Expanded(
+                                            child:
+                                                Text('${driver['year']}'),
+                                          ),
+                                          Expanded(
+                                            child: Text(
+                                                '${driver['name']}'),
+                                          ),
+                                          Expanded(
+                                            child: Text(
+                                                '${driver['nationality']}'),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  ),
+                                ),
+                                actions: [
+                                  TextButton(
+                                    child: const Text('Cerrar'),
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ],
+                              );
+                            },
+                          );
+                        }
+                      },
+                      child: const Text('Consulta'),
                     ),
                   ],
                 ),
@@ -107,6 +216,16 @@ class _MyAppState extends State<MyApp> {
                     );
                   },
                 ),
+              ),
+            if (!showButton)
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    showButton = true;
+                    drivers.clear();
+                  });
+                },
+                child: const Text('Regresar para elegir otro año'),
               ),
           ],
         ),
